dd15a34b6adc1fe9cdf0f2195476a3f41e1d7020
galljo <galljo@ufl.edu>
2017-09-26 13:53:33 -0400

    this is another commit

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String args[]){
+
+        double x = 4;
+        double y = 9;
+
+        if ((x + y) > 15) {
+            System.out.println("wow that's a big number");
+        }
+        else {
+            System.out.println("that's a small number...");
+        }
+    }
+}
